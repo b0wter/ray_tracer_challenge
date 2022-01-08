f981e7b00d799d1a824dc6085978eedb8ff76af5
b0wter <b0wter@gmx.de>
2022-01-08 14:13:53 +0100

    Added PPM module

--- src/PPM.fs
+++ src/PPM.fs
@@ -1,2 +1,34 @@
-module RayTracer.Challenge.PPM
+namespace RayTracer.Challenge
 
+open System
+
+module PPM =
+    let header width height =
+        [ "P3"
+          $"%i{width} %i{height}"
+          "255"
+        ]
+        
+
+    let color (c: Color.Color) : string =
+        let r = (255.0 * c.R) |> int
+        let g = (255.0 * c.G) |> int
+        let b = (255.0 * c.B) |> int
+        $"%i{r} %i{g} %i{b}"
+        
+        
+    let body (points: Color.Color[,]) =
+        let createRow (row: Color.Color []) : string =
+            row |> Array.fold (fun acc next -> acc + " " + (next |> color)) String.Empty
+        points
+        |> Array2D.asRows
+        |> List.map createRow
+    
+    
+    let fromCanvas (canvas: Canvas.Canvas<Color.Color>) =
+        let width = canvas.Points |> Array2D.width
+        let height = canvas.Points |> Array2D.height
+        let asLines = (header width height) @ (body canvas.Points)
+        String.Join(Environment.NewLine, asLines)
+    
+    
