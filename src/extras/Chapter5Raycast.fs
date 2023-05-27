bad5482f8ca1c1da9bbc1598e76984c4b964682c
b0wter <b0wter@gmx.de>
2023-05-27 23:57:35 +0200

    Added parallelization to the extra challenge

--- src/extras/Chapter5Raycast.fs
+++ src/extras/Chapter5Raycast.fs
@@ -1,6 +1,7 @@
 namespace Raytracer.Extras
 
 open System
+open System.Threading.Tasks
 open RayTracer.Challenge
 
 module Chapter5Raycast =
@@ -18,22 +19,43 @@ module Chapter5Raycast =
            |-- X -->
     *)
     
-    let writeRaycastToFile (filename: string) =
-        let canvas = Canvas.create edgeLength edgeLength Color.blue
-        let canvasDistance = 100.0
-        let sphere = Spheres.create ()
-        let camera = Tuple.createPoint (-2.0, 0.0, 0.0)
-        
-        for y in 0..edgeLength - 1 do
-            for z in 0..edgeLength - 1 do
-                let ray = Rays.create (camera, Tuple.createVector(canvasDistance, y, z))
-                let intersections = Intersections.intersectionsWith (Intersections.Sphere sphere) ray
-                if intersections |> Intersections.hit |> Option.isSome then
-                    Canvas.setPixel z y Color.red canvas
-                else
-                    Canvas.setPixel z y Color.black canvas
-                
-        let ppm = canvas |> PPM.fromCanvas
-        do System.IO.File.WriteAllText(filename, ppm)
+    let writeRaycastToFile (degreeOfParallelization: int) (filename: string) =
+        task {
+            let canvas = Canvas.create edgeLength edgeLength Color.blue
+            let canvasDistance = 100.0
+            let sphere =
+                Spheres.create ()
+                |> Spheres.withTransform (Matrices.translation (0.0, 513.0, 512.0))
+            let camera = Tuple.createPoint (-2.0, 512.0, 512.0)
             
+            let drawRange (startY: int) (endY: int) =
+                for y in startY..endY - 1 do
+                    for z in 0..edgeLength - 1 do
+                        let ray = Rays.create (camera, Tuple.createVector(canvasDistance, y, z))
+                        let intersections = Intersections.intersectionsWith (Intersections.Sphere sphere) ray
+                        try
+                            if intersections |> Intersections.hit |> Option.isSome then
+                                Canvas.setPixel z y Color.red canvas
+                            else
+                                Canvas.setPixel z y Color.black canvas
+                        with
+                        | _ ->
+                            printfn "%i - %i" z y
+                            
+            let drawRangeTask startY endY =
+                Task.Run(fun () -> drawRange startY endY)
+                            
+            let numberOfTasks = degreeOfParallelization
+            let taskRange = edgeLength / numberOfTasks
+            let tasks =
+                [0..numberOfTasks - 1]
+                |> List.map (fun i ->
+                    let start = i * taskRange
+                    let ``end`` = start + taskRange
+                    drawRangeTask start  ``end``)
                 
+            let! _ = Task.WhenAll tasks
+            
+            let ppm = canvas |> PPM.fromCanvas
+            do System.IO.File.WriteAllText(filename, ppm)
+        }
--- src/extras/Program.fs
+++ src/extras/Program.fs
@@ -1,6 +1,12 @@
-﻿open Raytracer.Extras
+﻿open System.Diagnostics
+open Raytracer.Extras
 
 //let chapter1Result = Chapter1Projectile.runPredefined ()
 //let chapter2Result = Chapter2Canvas.writePredefinedToFile "result.ppm"
-let chapter7Result = Chapter5Raycast.writeRaycastToFile "result.ppm"
+
+let watch = Stopwatch()
+watch.Start()
+let chapter7Result = (Chapter5Raycast.writeRaycastToFile 4 "result.ppm").GetAwaiter().GetResult()
+watch.Stop()
+printfn "%i" watch.ElapsedMilliseconds
 
